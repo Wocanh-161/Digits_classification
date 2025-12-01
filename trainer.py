a1cd775cbc4826a2799c8abadb29cd3b3b3e2607
bich1410 <steve.fred.c14@gmail.com>
2025-12-01 22:44:16 +0700

    Update trainer.py
    loss

--- trainer.py
+++ trainer.py
@@ -4,6 +4,7 @@ sys.path.append('src')  # Thêm thư mục src vào sys.path
 # trainer.py
 from models.model import CNN  # Import CNN model
 from dataloader import load_data  # Import data loader
+from loss import get_loss_function
 
 import torch
 import torch.optim as optim
@@ -20,3 +21,5 @@ def evaluate_model(model, testloader, device):
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     trainloader, valloader, testloader = load_data()
+    criterion = get_loss_function()
+
